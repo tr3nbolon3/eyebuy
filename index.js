2911e23f36b8dad9567585bf903ebe7d234fd504
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2020-10-02 12:06:29 +0600

    remove setTimeout

--- index.js
+++ index.js
@@ -4,33 +4,29 @@
   console.log('*************************************');
 
   const init = (options) => {
-    setTimeout(() => {
-      const addToCart = options.addToCart || (() => {});
-      const redirectUrl = options.redirectUrl || window.location.href;
+    const addToCart = options.addToCart || (() => {});
+    const redirectUrl = options.redirectUrl || window.location.href;
 
-      const urlObject = new URL(window.location.href);
-      const jsonProducts = urlObject.searchParams.getAll('products');
-      const products = jsonProducts.map(JSON.parse);
+    const urlObject = new URL(window.location.href);
+    const jsonProducts = urlObject.searchParams.getAll('products');
+    const products = jsonProducts.map(JSON.parse);
 
-      console.log('urlObject.search', urlObject.search);
-      document.cookie = `from_eyebuy=${urlObject.search}}`;
-      console.log('document.cookie', document.cookie);
+    document.cookie = `from_eyebuy=${urlObject.search}}`;
 
-      if (!products.length) {
-        window.location.replace(redirectUrl);
-        return;
-      }
+    if (!products.length) {
+      window.location.replace(redirectUrl);
+      return;
+    }
 
-      const promisesChain = products
-        .slice(1)
-        .reduce((acc, product) => {
-          return acc.then(() => addToCart(product));
-        }, Promise.resolve(addToCart(products[0])));
+    const promisesChain = products
+      .slice(1)
+      .reduce((acc, product) => {
+        return acc.then(() => addToCart(product));
+      }, Promise.resolve(addToCart(products[0])));
 
-      promisesChain.then(() => {
-        window.location.replace(redirectUrl);
-      });
-    }, 2000)
+    promisesChain.then(() => {
+      window.location.replace(redirectUrl);
+    });
   };
 
   window.eyebuy = init;
