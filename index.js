176187d289a36165cf59c698920ea1de45c32192
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2020-10-02 12:03:06 +0600

    update script

--- index.js
+++ index.js
@@ -4,33 +4,33 @@
   console.log('*************************************');
 
   const init = (options) => {
-    const addToCart = options.addToCart || (() => {});
-    const redirectUrl = options.redirectUrl || window.location.href;
-
-    const urlObject = new URL(window.location.href);
-    const jsonProducts = urlObject.searchParams.getAll('products');
-    const products = jsonProducts.map(JSON.parse);
-
-    console.log('document.cookie', document.cookie);
     setTimeout(() => {
+      const addToCart = options.addToCart || (() => {});
+      const redirectUrl = options.redirectUrl || window.location.href;
+
+      const urlObject = new URL(window.location.href);
+      const jsonProducts = urlObject.searchParams.getAll('products');
+      const products = jsonProducts.map(JSON.parse);
+
       console.log('urlObject.search', urlObject.search);
       document.cookie = `from_eyebuy=${urlObject.search}}`;
+      console.log('document.cookie', document.cookie);
+
+      if (!products.length) {
+        window.location.replace(redirectUrl);
+        return;
+      }
+
+      const promisesChain = products
+        .slice(1)
+        .reduce((acc, product) => {
+          return acc.then(() => addToCart(product));
+        }, Promise.resolve(addToCart(products[0])));
+
+      promisesChain.then(() => {
+        window.location.replace(redirectUrl);
+      });
     }, 2000)
-
-    if (!products.length) {
-      window.location.replace(redirectUrl);
-      return;
-    }
-
-    const promisesChain = products
-      .slice(1)
-      .reduce((acc, product) => {
-        return acc.then(() => addToCart(product));
-      }, Promise.resolve(addToCart(products[0])));
-
-    promisesChain.then(() => {
-      window.location.replace(redirectUrl);
-    });
   };
 
   window.eyebuy = init;
